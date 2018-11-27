5e67e32c227769ac783e0e58ad27e9775c53a763
fzitou <jzitou@gmail.com>
2018-11-27 16:50:12 +0800

    config uuid

--- v2aru/web/entrypoint.sh
+++ v2aru/web/entrypoint.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 # echo -e -n "$CONF" > /etc/v2ray/config.json
 
+sed -ri "s/28807b19-64ac-431b-8f91-2e87b2aa63b2/$UUID/g" /etc/v2ray/config.json
+
 supervisord -c /etc/supervisord.conf
