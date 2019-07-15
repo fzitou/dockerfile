01e95b346c3039f4bfef560cede845931a1c19b2
fzitou <jzitou@gmail.com>
2019-07-15 18:29:31 +0800

    customize your dns now

--- v2aru/nginx/entrypoint.sh
+++ v2aru/nginx/entrypoint.sh
@@ -2,5 +2,6 @@
 # echo -e -n "$CONF" > /etc/v2ray/config.json
 
 sed -ri "s/28807b19-64ac-431b-8f91-2e87b2aa63b2/$UUID/g" /etc/v2ray/config.json
+sed -ri "s/8.8.8.8/$DNS/g" /etc/v2ray/config.json
 
 supervisord -c /etc/supervisord.conf
